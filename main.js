d547f48f312ce535d890270f88f38602c5cbe3e1
yanna Lee <yannalee@192.168.0.106>
2017-12-03 00:09:31 +0800

    天数改成大写

--- main.js
+++ main.js
@@ -4,4 +4,4 @@
 var startDay = new Date("October 26, 2017 9:00:00");
 var difference = Math.floor((new Date() - startDay)/(1000 * 60 * 60 *24));
 var displayDay = document.getElementById('day');
-displayDay.innerHTML = `${difference} Days`;
+displayDay.innerHTML = `${difference} DAYS`;
