ec4c914561bca93ffaf240fa4302951f8baf9e36
jacklee <888jacklee@gmail.com>
2017-12-02 23:52:00 +0800

    added days counter

---
+++ main.js
@@ -0,0 +1,7 @@
+// current day - start day and display to html
+
+// var currentDay = new Date();
+var startDay = new Date("October 26, 2017 9:00:00");
+var difference = Math.floor((new Date() - startDay)/(1000 * 60 * 60 *24));
+var displayDay = document.getElementById('day');
+displayDay.innerHTML = `${difference} Days`;
